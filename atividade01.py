aebb24d6e2e30893734b04dd08959dfc6b1a8d1d
Iagosilvabrito <iago.silvabrito1@outlook.com>
2023-08-28 17:09:41 -0300

    Enviando primeira Atividade

---
+++ atividade01.py
@@ -0,0 +1,10 @@
+#sucessor e antecessor de um numero
+
+num = int(input("escreva um numero: "))
+antecessor = num - 1 
+sucessor = num + 1
+
+print(f"O numero é {num} e o sucessor é {sucessor} e o antecessor é {antecessor}.")
+
+
+
